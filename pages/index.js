7219e5d62fc4c905a0ec4fa9a9d47350180021d0
Lucas Dias <lucasdias067@gmail.com>
2022-11-17 15:43:07 -0300

    commiting

---
+++ pages/index.js
@@ -0,0 +1,106 @@
+import config from '../config.json';
+import styled from 'styled-components';
+import Menu from '../src/components/Menu';
+import { StyledTimeline } from '../src/styles/Timeline';
+import Banner from '../src/assets/Banner.jpg'
+import React from 'react';
+import { Reset } from '../src/components/Menu/Reset';
+import { videoService } from '../src/services/videoServices';
+
+function HomePage() {
+  const [valorDoFiltro, setValorDoFiltro] = React.useState('');
+  const [playlists, setPlaylists] = React.useState({});
+  const service = videoService();
+
+  React.useEffect(() => {
+    service
+      .getAllVideos(setPlaylists)
+  }, []);
+
+  return (
+    <>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        flex: 1,
+      }}>
+        <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+        <Header />
+        {Object.keys(playlists).length
+          ? <Timeline playlists={playlists} valorDoFiltro={valorDoFiltro} />
+          : <Timeline playlists={config.playlists} valorDoFiltro={valorDoFiltro} />}
+        <Reset />
+      </div>
+    </>
+  )
+}
+
+export default HomePage
+
+const StyledHeader = styled.div`
+  background-color: ${({ theme }) => theme.backgroundLevel1};
+
+  img{
+    width: 100%;
+    height: 250px;
+  }
+
+  section img{
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+
+  .user-info{
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+  }
+`;
+
+function Header() {
+  return (
+    <StyledHeader>
+      <img src={Banner.src} width={Banner.width} height={Banner.height} />
+      <section className='user-info'>
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>{config.name}</h2>
+          <p>{config.description}</p>
+        </div>
+      </section>
+    </StyledHeader>
+  )
+} config
+
+function Timeline({ playlists, valorDoFiltro }) {
+  const playlistNames = Object.keys(playlists)
+  return (
+    <StyledTimeline>
+      {playlistNames.map(playlistName => {
+        const videos = playlists[playlistName]
+        return (
+          <section key={playlistName}>
+            <h2>{playlistName}</h2>
+            <div>
+              {videos.filter(video => {
+                return video.title.toLowerCase().includes(valorDoFiltro.toLowerCase())
+              }).map(video => {
+                return (
+                  <a href={video.url} key={video.url}>
+                    <img src={video.thumb} alt="video" />
+                    <span>
+                      {video.title}
+                    </span>
+                  </a>
+                )
+              })}
+            </div>
+          </section>
+        )
+      })}
+    </StyledTimeline>
+  )
+}
---
+++ src/components/RegisterVideo/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { StyledRegisterVideo } from "./styles";
+import { videoService } from '../../services/videoServices.js';
+import { createClient } from "@supabase/supabase-js";
+
+function useForm({ initialValues }) {
+  const [values, setValues] = React.useState(initialValues);
+
+  return {
+    values, handleChange(e) {
+      const value = e.target.value;
+      const name = e.target.name;
+      setValues({ ...values, [name]: value });
+    },
+    clearForm() {
+      setValues({});
+    }
+  }
+}
+
+export default function RegisterVideo() {
+  const form = useForm({ initialValues: { titulo: '', url: '', playlist: '',} });
+  const [formVisivel, setformVisivel] = React.useState(false);
+  const PROJECT_URL = 'https://fwrkrlbgrdsacsjzjyfp.supabase.co'
+  const PUBLIC_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cmtybGJncmRzYWNzanpqeWZwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzODk1OTIsImV4cCI6MTk4Mzk2NTU5Mn0.TNKw_ndwn1A4rWkNu-0tNCfjKWJ72swLcBCDFKU79o4'
+  const supabase = createClient(PROJECT_URL, PUBLIC_KEY)
+  const service = videoService();
+
+  function displayForm() {
+    setformVisivel(!formVisivel)
+  }
+
+  return (
+    <StyledRegisterVideo>
+      <button className="add-video" onClick={displayForm}>+</button>
+      {formVisivel && (
+        <form onSubmit={(e) => {
+          e.preventDefault();
+
+          supabase.from('video').insert({
+            title: form.values.titulo,
+            url: form.values.url,
+            thumb: service.getThumbnail(form.values.url),
+            playlist: form.values.playlist
+          })
+            .then(result => {
+              console.log(result)
+            })
+            .catch(err => {
+              console.log(err)
+            })
+
+          displayForm();
+          form.clearForm()
+
+        }}>
+          <div>
+            <div>
+              <a href={form.values.url} key={form.values.url}>
+                <img src={service.getThumbnail(form.values.url)} alt="video" />
+              </a>
+            </div>
+            <button type="button" className="close-modal" onClick={displayForm}>X</button>
+            <input placeholder="Titulo do vídeo" name="titulo" value={form.values.titulo} onChange={form.handleChange} required />
+            <input placeholder="URL" value={form.values.url} name="url" onChange={form.handleChange} required />
+            <input placeholder="Playlist" value={form.values.playlist} name="playlist" onChange={form.handleChange} required />
+            <button type="submit">Cadastrar</button>
+          </div>
+        </form>
+      )}
+    </StyledRegisterVideo>
+  )
+}      
